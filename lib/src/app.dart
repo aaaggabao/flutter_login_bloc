cee59b3f5598c567826e612218dd334a1dcc594b
aaaggabao <34728532+aaaggabao@users.noreply.github.com>
2019-03-11 13:36:18 +0800

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_login_bloc/src/blocs/provider.dart';
+import 'package:flutter_login_bloc/src/screens/login_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      )
+    );
+  }
+
+}
